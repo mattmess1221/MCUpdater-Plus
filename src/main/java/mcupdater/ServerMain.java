b5a79a34be2be6a686f2ea495f8bf7456a91a8d7
killjoy1221 <mattmess1221@gmail.com>
2014-05-20 02:38:37 -0400

    Server support. Move to server directory and script to run before Forge.

---
+++ src/main/java/mcupdater/ServerMain.java
@@ -0,0 +1,54 @@
+package mcupdater;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Method;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+
+
+public class ServerMain {
+
+	public static void main(final String[] args) throws Exception {
+		new ServerMain().loadMCJar();
+		Thread thread = new Thread("Updater"){
+			public void run(){
+				new UpdaterMain().main(args);
+			}
+		};
+		thread.start();
+	}
+
+	private void loadMCJar() throws IOException{
+		System.out.println("Loading Libraries...");
+		for(File file : new File(System.getProperty("user.dir")).listFiles()){
+			if(file.getName().startsWith("minecraft_server") && file.getName().endsWith(".jar")){
+				addURL(file.toURI().toURL());
+			} else if (file.isDirectory() && file.getName().equals("libraries")){
+				recursiveLookup(file);
+			}
+		}
+	}
+	
+	private void recursiveLookup(File file) throws MalformedURLException{
+		for(File file1 : file.listFiles()){
+			if(file1.isDirectory())
+				recursiveLookup(file1);
+			else
+				addURL(file1.toURI().toURL());
+		}
+	}
+	
+	private void addURL(URL u){
+		URLClassLoader classLoader = (URLClassLoader) ClassLoader.getSystemClassLoader();
+		Class<URLClassLoader> clClass = URLClassLoader.class;
+		try {
+			Method method = clClass.getDeclaredMethod("addURL", URL.class);
+			method.setAccessible(true);
+			method.invoke(classLoader, u);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}

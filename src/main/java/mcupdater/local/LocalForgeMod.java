f49c33e6792a9a26ad40dab9d5ecb97cf46b9cbf
killjoy1221 <mattmess1221@gmail.com>
2014-06-01 16:21:11 -0400

    Support for mods that use renamed mcmod.info
    I'm looking at you, Chicken Bones.

--- src/main/java/mcupdater/local/LocalForgeMod.java
+++ src/main/java/mcupdater/local/LocalForgeMod.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.Enumeration;
+import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 
 import com.google.gson.JsonArray;
@@ -14,14 +16,23 @@ public class LocalForgeMod extends LocalMod{
 	public LocalForgeMod(File file) throws IOException {
 		this.file = file;
 		ZipFile zip = new ZipFile(file);
-		InputStream mcmod = zip.getInputStream(zip.getEntry("mcmod.info"));
+		InputStream mcmod = null; //zip.getInputStream(zip.getEntry("mcmod.info"));
+		Enumeration<? extends ZipEntry> entries = zip.entries();
+		while(entries.hasMoreElements()){
+			ZipEntry entry = entries.nextElement();
+			if(entry.getName().endsWith("mod.info")){
+				mcmod = zip.getInputStream(entry);
+				break;
+			}
+			throw new IOException("mcmod.info not found!");
+		}
 		JsonArray array = gson.fromJson(new InputStreamReader(mcmod), JsonArray.class);
 		JsonObject object = array.get(0).getAsJsonObject();
-		if (mcmod != null) {
-			this.name = object.get("name").getAsString();
-			this.modid = object.get("modid").getAsString();
-			this.version = object.get("version").getAsString();
-		}
+
+		this.name = object.get("name").getAsString();
+		this.modid = object.get("modid").getAsString();
+		this.version = object.get("version").getAsString();
+		
 		mcmod.close();
 		zip.close();
 	}
